5f03e822b0b73e56d75bac381d4d617ed7a90e74
parvej khan <1999monustp@gmail.com>
2024-12-12 20:59:28 +0530

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Nav from './Nav'
 function App() {
 
   return (
     <>
+      <Nav></Nav>
     </>
   )
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,6 @@
+function Nav(props) {
+    return (<div>
+        hi this is me
+    </div>)
+}
+export default Nav
