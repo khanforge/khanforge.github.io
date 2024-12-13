da5d061b70c7b9bba708379671584ac1eaf7c95e
parvej khan <1999monustp@gmail.com>
2024-12-13 20:28:33 +0530

    added the logo and navbar working

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,6 +1,28 @@
+import { NavItems } from "./assets/NavItems.js"
+import { useState } from "react"
 function Nav(props) {
-    return (<div>
-        hi this is me
-    </div>)
+    const [isActive, setIsActive] = useState();
+    const handleIsActive = (id) => {
+        setIsActive(id);
+        console.log(id);
+    }
+    return (
+        <div className="flex w-full justify-around h-14 items-center text-white px-10 mt-2">
+            <div id="logo" className="font-extrabold text-2xl h-12 w-12">
+                <img src="./logo.png" className="text-white h-12 w-12" alt="" srcset="" />
+            </div>
+            <ul className="flex justify-end  w-full">
+                {NavItems.map((item, index) => (
+                    <li id="link"
+                        className={`mr-5 ${item.id === isActive && 'border-b-2 border-yellow-100'} cursor-pointer px-2 py-1`}
+                        key={index}
+                        onClick={() => handleIsActive(item.id)}
+                    >
+                        {item.title}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 export default Nav
---
+++ src/assets/NavItems.js
@@ -0,0 +1,9 @@
+export const NavItems = [
+    { id: 0, title: "Home" },
+    { id: 1, title: "About" },
+    { id: 2, title: "Work Experience" },
+    { id: 3, title: "Skills" },
+    { id: 4, title: "Resume/CV" },
+    { id: 5, title: "Blog" },
+    { id: 6, title: "Contact" },
+];
