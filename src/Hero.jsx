2516f640be762f18cca9cf4393e007dbfb140fbd
parvej khan <1999monustp@gmail.com>
2024-12-15 13:23:33 +0530

    outline class added

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,11 +2,11 @@ const Hero = (props) => {
     return (
         <>
             <section id="hero" className="flex mt-40 w-full justify-around">
-                <div id="tagline" className="text-slate-50 flex flex-col">
-                    <span className="text-[3rem]">Full Stack Web-Developer</span>
-                    <div className="text-[6rem] font-extrabold flex flex-col">
-                        <span className="">Parvej </span>
-                        <span>Khan</span>
+                <div id="tagline" className=" flex flex-col">
+                    <span className="text-[3rem] text-slate-50">Full Stack Web-Developer</span>
+                    <div className="text-[6rem] font-extrabold flex flex-col gap-y-0 leading-none">
+                        <span className="text-outline-2">Parvej </span>
+                        <span className="text-slate-50">Khan</span>
                     </div>
 
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,19 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-outline': {
+          '-webkit-text-stroke': '1px #f5a3a3', /* Light pink stroke */
+          'color': 'transparent',              /* Makes the fill transparent */
+        },
+        '.text-outline-2': {
+          '-webkit-text-stroke': '2px #f5a3a3', /* Thicker stroke variant */
+          'color': 'transparent',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
+};
